fbe209fbc8f74d751f7358ad46964514b1b8f986
Paul W Roberson <probers1@gmail.com>
2025-10-18 10:52:40 -0500

    Remove frozen-lockfile option from pnpm commands

--- ct/immich.sh
+++ ct/immich.sh
@@ -165,10 +165,10 @@ EOF
 
     # server build
     export SHARP_IGNORE_GLOBAL_LIBVIPS=true
-    $STD pnpm --filter immich --frozen-lockfile build
+    $STD pnpm --filter immich  build
     unset SHARP_IGNORE_GLOBAL_LIBVIPS
     export SHARP_FORCE_GLOBAL_LIBVIPS=true
-    $STD pnpm --filter immich --frozen-lockfile --prod --no-optional deploy "$APP_DIR"
+    $STD pnpm --filter immich  --prod --no-optional deploy "$APP_DIR"
     cp "$APP_DIR"/package.json "$APP_DIR"/bin
     sed -i 's|^start|./start|' "$APP_DIR"/bin/immich-admin
 
@@ -183,7 +183,7 @@ EOF
     cp LICENSE "$APP_DIR"
 
     # cli build
-    $STD pnpm --filter @immich/sdk --filter @immich/cli --frozen-lockfile install
+    $STD pnpm --filter @immich/sdk --filter @immich/cli  install
     $STD pnpm --filter @immich/sdk --filter @immich/cli build
     $STD pnpm --filter @immich/cli --prod --no-optional deploy "$APP_DIR"/cli
     cd "$APP_DIR"
